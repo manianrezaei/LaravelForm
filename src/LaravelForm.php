0d07f10caf86a7f45157e7855c47ba219d51cef5
Manian <Manianrezaie@gmail.com>
2019-10-12 13:30:12 +0330

    First commit for start

---
+++ src/LaravelForm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vartan
+ * Date: 10/12/2019
+ * Time: 1:28 PM
+ */
+
+namespace Manian\LaravelForm;
+
+class LaravelForm
+{
+    public static function test()
+    {
+        return "Hello World!";
+    }
+}
---
+++ src/LaravelFormServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+// MyVendor\contactform\src\ContactFormServiceProvider.php
+namespace Manian\LaravelForm;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFormServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
+
+?>
