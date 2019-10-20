1b774747c33ab96ec097723b7e3e228493cc66f6
Manian <Manianrezaie@gmail.com>
2019-10-20 16:04:07 +0330

    clean up

--- src/LaravelForm.php
+++ src/LaravelForm.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vartan
- * Date: 10/12/2019
- * Time: 1:28 PM
- */
+
 
 namespace Manian\LaravelForm;
-
 class LaravelForm
 {
     public static function test()
